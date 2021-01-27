912e81044623be62c6cc45d55d4bcf949fdf1933
enzokid <overstill77@gmail.com>
2021-01-27 14:56:58 +0200

    if/else/switch

--- TestProject/src/mypackage/TestClass.java
+++ TestProject/src/mypackage/TestClass.java
@@ -7,40 +7,52 @@ public class TestClass {
 
     public static void main(String[] args) {
 
+        int i = 10;
+        System.out.println("Start of program");
+        if (i > 10) {
+            System.out.println("i>10");
+            i++;
+            System.out.println(i);
+        }
+        else if (i < 10 && i > 5) {
+            System.out.println("i < 10 && i > 5");
+            i--;
+            System.out.println(i);
+        }
+        else {
+            System.out.println("Nothing");
+        }
 
-        String s = "Hello,World";
-
+        System.out.println("End of program");
         System.out.println();
-        String[] array = s.split(",");
-        System.out.println(array[0] + " !" + array[1] + "!");
 
-        System.out.println(array[0]);
-        System.out.println(array[1]);
+        i=95;
+        if(i>50){
+            System.out.println("i>50");
+            if (i>90){
+                System.out.println("i>90");
+            }
+            else{
+                System.out.println("i<=90");
+            }
+        }
+        else{
+            System.out.println("i<=50");
 
-        String str = "My name is %s! I'm %d years old!";
-        int age = 30;
-        String name = "Ivan";
-        System.out.println(String.format(str, name, age));
-        System.out.println(String.format(str, "Vasja", 10));
 
-        System.out.println();
-        String age2 = "10";
-        int a = Integer.parseInt(age2);
-        System.out.println(a*3);
 
-        s = "Hello, world!";
-        System.out.println(s.substring(7));
-        System.out.println(s.substring(7, s.length()- 2));
+        }
 
-        String s1 = "Hello ";
-        String s2 = "world";
-        String s3 = "!";
-        String res;
-        res= s1+s2+s3;
-        System.out.println(res);
-        res= s1.concat(s2).concat(s3);
-        System.out.println(res);
-
+        i=10;
+        switch (i){
+            case 1:
+                System.out.println("The number is 1");
+                break;
+            case 2:
+                System.out.println("The number is 2");
+                break;
+            default:
+                System.out.println("I don't know the number");
+        }
     }
-
 }
