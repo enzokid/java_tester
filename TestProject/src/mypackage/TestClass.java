5240e976451e44a4f81998eaf237382ce7f6d68d
enzokid <overstill77@gmail.com>
2021-02-03 12:37:57 +0200

    List, iterator

--- TestProject/src/mypackage/TestClass.java
+++ TestProject/src/mypackage/TestClass.java
@@ -1,46 +1,80 @@
 package mypackage;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class TestClass {
 
 
     public static void main(String[] args) {
-        System.out.println("Start program");
-        for (int i=0; i<10; i++){
-            System.out.println(i*10);
-            System.out.println();
-        }
-        for (int i=10; i>0; i--){
-            System.out.println(i);
-        }
-        int[] array= new int[10];
-        for(int i=0; i<10; i++){
-            array[i]=i*2;
-        }
-        for(int i=0; i<10; i++){
-            System.out.println("Element with index " +i+ " is: " + array[i]);
-        }
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(9);
 
-        for(int element: array){
-            System.out.println(element);
+        int a = list.get(2);
+        System.out.println(list.get(1));
+        System.out.println(list.get(0));
+        System.out.println(a);
 
-        }
-        int i=0;
-        boolean bool= true;
-        while(bool){
-            System.out.println("while : " +i);
-            i++;
-            if(i==5) bool=false;
-        }
+        list.set(2, 100);
+        System.out.println(list.get(2));
+
+        System.out.println();
+        System.out.println(list.get(1));
+        list.remove(1);
+        System.out.println(list.get(1));
+
+        System.out.println();
+        System.out.println(list.size());
+        list.add(9);
+        System.out.println(list.size());
+
+//      list.clear();
+        System.out.println(list.size());
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(45);
+        list2.add(56);
+
         System.out.println();
 
-        i=5;
-        do{
-            System.out.println("Do: "+i);
-            i++;
+        System.out.println(list.size());
+        list.addAll(list2);
+        System.out.println(list.size());
+        list.removeAll(list2);
+        System.out.println(list.size());
+
+        System.out.println(list.isEmpty());
+//        list.clear();
+        System.out.println(list.isEmpty());
+
+
+        System.out.println(list.contains(100));
+        System.out.println(list.contains(987));
+
+        System.out.println();
+
+        list.addAll(list2);
+        System.out.println(list.containsAll(list2));
+
+        list.removeAll(list2);
+        System.out.println(list.containsAll(list2));
+
+
+        Iterator<Integer> iterator = list.iterator();
+        iterator.next();
+//        System.out.println(iterator.next());
+ //       System.out.println(iterator.next());
+ //       System.out.println(iterator.hasNext());
+        System.out.println();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
         }
-        while (i<5);
-        System.out.println("End program");
+
+
+
     }
 }
