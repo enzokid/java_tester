aab1126d0dfa301998cd05724fd175b66b81090c
enzokid <overstill77@gmail.com>
2021-02-05 11:38:19 +0200

    Selenium webDriver (driver.manage())

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainClass {
 
@@ -8,7 +11,21 @@ public class MainClass {
 
         WebDriver driver = new FirefoxDriver();
 
-        driver.get("https://www.selenium.dev/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.manage().window().setSize(new Dimension(900, 500));
+
+        driver.get("https://www.google.com/");
+        driver.navigate().to("http://seleniumhq.org");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        driver.quit();
+
     }
 
 }
