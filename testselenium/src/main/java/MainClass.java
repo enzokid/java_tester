572054d8460a60b95ba5c9c96f6565817e3a4744
enzokid <overstill77@gmail.com>
2021-02-16 22:10:03 +0200

    Links

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -15,28 +15,14 @@ public class MainClass {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-//      driver.get("http://en.wikipedia.org");
-//        driver.findElement(By.xpath("//input[@id='searchButton']")).click();
+        driver.get("http://en.wikipedia.org");
+        WebElement link = driver.findElement(By.xpath("//li[@id='n-aboutsite']/a"));
+        System.out.println(link.getText());
+        link.click();
 
 
-//        driver.get("http://github.com");
-//        WebElement button = driver.findElement(By.xpath("//form[@class='mx-auto mx-md-0 col-5-max js-signup-form position-relative z-2']//button"));
-//        if(button.getText().equals("Sign up for GitHub")) {
-//            System.out.println("Success!!");
-//        }
-//        else System.out.println("Fail!");
+        
 
-//        button.submit();
-
-//        driver.findElement(By.xpath("//a[@href='/login']")).click();
-
-        driver.get("http://en-gb.facebook.com");
-
-        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("testmail");
-        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("testpass");
-        driver.findElement(By.xpath("//button[@id=\"u_0_j_VD\"]")).click();
-
-        driver.findElement(By.xpath("//button[@id=\"u_0_d_NV\"]")).submit();
 
 //      driver.quit();
 
