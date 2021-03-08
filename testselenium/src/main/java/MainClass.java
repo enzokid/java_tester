9978b2f1a9fcabb69ee21d498c982b71fb3dd14c
enzokid <overstill77@gmail.com>
2021-03-08 12:17:49 +0200

    work with tables

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -10,25 +12,26 @@ import java.util.concurrent.TimeUnit;
 public class MainClass {
 
     static WebDriver driver;
+    static  WebDriverWait wait;
+
 
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\enzokid\\Documents\\GitHub\\java_tester\\testselenium\\drivers\\geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.get("https://www.w3schools.com/html/html_tables.asp");
+        driver.get("https://tele2.ee/ru/paketid/mobiilipaketid?gclid=EAIaIQobChMI_MnkipeD7wIVAaOyCh1Nhg7kEAAYASAAEgJDCfD_BwE");
 
-        WebElement tableElement = driver.findElement(By.xpath("//table[@id=\"customers\"]"));
+        wait = new WebDriverWait(driver, 5);
 
-        Table table = new Table(tableElement, driver);                          // конструктор (создаём таблицу)
-        System.out.println("Rows number is "+ table.getRows().size());
-        System.out.println(table.getValueFromCell(2, 3));
-        System.out.println(table.getValueFromCell(4, 1));
-        System.out.println(table.getValueFromCell(4, "Company"));
-        System.out.println(table.getValueFromCell(1, "Country"));
-        System.out.println(table.getValueFromCell(2, "Contact"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]")));
+
+        driver.findElement(By.xpath("//a[@id=\"CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll\"]")).click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div[1]")));
+
+
 
 
 
